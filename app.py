ec7e185ca403ea4abec44ed4457a96d7ee2d0ff0
Henrique C. C. de Andrade <henrique.ccda@gmail.com>
2025-07-11 22:25:01 -0300

    Add seleção de usuario.

--- app.py
+++ app.py
@@ -3,12 +3,43 @@ from fastapi.responses import RedirectResponse
 import uuid
 import jwt
 from datetime import datetime, timedelta
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
 # Armazenamento em memória
 codes_storage = {}
 SECRET_KEY = "mysecretkey"
+
+templates = Jinja2Templates(directory="templates")
+
+users = {
+    "AAAA": {
+        "given_name": "Fernando",
+        "email": "fernando@example.com",
+        "department": "LACEO",
+    },
+    "BBBB": {
+        "given_name": "Henrique",
+        "email": "henrique@example.com",
+        "department": "LABEST",
+    },
+    "CCCC": {
+        "given_name": "Gabriela",
+        "email": "gabi@example.com",
+        "department": "LACEO",
+    },
+    "DDDD": {
+        "given_name": "Guilherme",
+        "email": "gui@example.com",
+        "department": "LACEO",
+    },
+    "EEEE": {
+        "given_name": "Manoel",
+        "email": "manoel@example.com",
+        "department": "LABEST",
+    }
+}
 
 # Rota de autorização
 @app.get("/oauth2/authorize")
@@ -33,11 +64,13 @@ async def authorize(
         "redirect_uri": uri_to_use,
         "scope": scope
     }
-    return RedirectResponse(f"{redirect_uri}?code={code}")
+
+    return RedirectResponse(f"/oauth2/login?code={code}&redirect_uri={redirect_uri}")
 
 # Rota de token
 @app.post("/oauth2/token")
 async def token(
+    request: Request,
     code: str = Form(...),
     client_id: str = Form(...),
     client_secret: str = Form(...),
@@ -45,6 +78,7 @@ async def token(
     redirect_uri: str = Form(...),
     grant_type: str = Form(...)
 ):
+
     if code not in codes_storage:
         raise HTTPException(400, "Código inválido")
 
@@ -53,11 +87,10 @@ async def token(
     refresh_token = str(uuid.uuid4())
 
     # Criar JWT
+    user_key = codes_storage[code]['user_key']
     payload = {
-        "user_login": "BBBB",
-        "given_name": "Henrique",
-        "email": "henrique@example.com",
-        "department": "LACEO",
+        **users[user_key],
+        "user_login": user_key,
         "exp": datetime.utcnow() + timedelta(seconds=3600)
     }
     id_token = jwt.encode(payload, SECRET_KEY, algorithm="HS256")
@@ -70,3 +103,28 @@ async def token(
         "expires_in": 3600,
         "id_token": id_token
     }
+
+
+@app.get("/oauth2/login")
+async def login_get(request: Request, code: str, redirect_uri: str):
+
+    return templates.TemplateResponse(
+        request=request,
+        name="login.jinja",
+        context={
+            "request": request,
+            "code": code,
+            "redirect_uri": redirect_uri,
+            "users": users,
+        }
+    )
+
+
+@app.post("/oauth2/login")
+async def login_post(
+    user_key: str = Form(...),
+    code: str = Form(...),
+    redirect_uri: str = Form(...),
+):
+    codes_storage[code] |= { "user_key": user_key }
+    return RedirectResponse(f"{redirect_uri}?code={code}")
